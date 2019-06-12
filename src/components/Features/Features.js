9c7d48d665b6e9e5ea00afe2ba91759c9483581a
jopasdev <joao@ople.ai>
2019-06-12 17:52:03 -0300

    adding correlation stats and template update

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -1,14 +1,15 @@
-import * as mathjs from 'mathjs'
+import { values, attributes } from '@/assets/descriptions.js'
 
-import { values, atributes } from '@/assets/descriptions.js'
+var jStat = require('jstat').jStat
 
 export default {
   props: ['indices'],
   data () {
     return {
-      atributes: atributes[0].split(','),
+      attributes: attributes[0].split(','),
       values: [],
-      atribute: null
+      attribute_A: '',
+      attribute_B: ''
     }
   },
   mounted () {
@@ -20,26 +21,69 @@ export default {
     statistics () {
       let stats = {}
       if (
-        this.atribute !== null &&
+        this.attribute_A !== '' &&
         Array.isArray(this.indices) &&
         this.indices.length
       ) {
-        var columnValues = this.indices.map(i => this.values[i][this.atribute])
+        var attributeValues = this.indices.map(i =>
+          Number(this.values[i][this.attribute_A])
+        )
         stats = {
-          mean: mathjs.mean(columnValues),
-          median: mathjs.median(columnValues),
-          std_deviation: mathjs.std(columnValues),
-          correlation: null,
+          mean: jStat.mean(attributeValues).toFixed(2),
+          median: jStat.median(attributeValues).toFixed(2),
+          std_deviation: jStat.stdev(attributeValues).toFixed(2),
           distribution: null,
           image_url: null
         }
       }
       return stats
-    }
-  },
-  methods: {
-    selectColumn (atribute) {
-      this.atribute = atribute
+    },
+    statistics_total () {
+      let stats = {}
+      if (this.attribute_A !== '') {
+        var attributeValues = this.values.map(value =>
+          Number(value[this.attribute_A])
+        )
+        stats = {
+          mean: jStat.mean(attributeValues).toFixed(2),
+          median: jStat.median(attributeValues).toFixed(2),
+          std_deviation: jStat.stdev(attributeValues).toFixed(2),
+          distribution: null,
+          image_url: null
+        }
+      }
+      return stats
+    },
+    statistics_correlation () {
+      let correlation = null
+      if (
+        this.attribute_A !== '' &&
+        this.attribute_B !== '' &&
+        Array.isArray(this.indices) &&
+        this.indices.length
+      ) {
+        var attributeValues = this.indices.map(i =>
+          Number(this.values[i][this.attribute_A])
+        )
+        var targetValues = this.indices.map(i =>
+          Number(this.values[i][this.attribute_B])
+        )
+        correlation = jStat.corrcoeff(attributeValues, targetValues).toFixed(2)
+      }
+      return correlation
+    },
+    statistics_correlation_total () {
+      let correlation = null
+      if (this.attribute_A !== '' && this.attribute_B !== '') {
+        var attributeValues = this.values.map(value =>
+          Number(value[this.attribute_A])
+        )
+        var targetValues = this.values.map(value =>
+          Number(value[this.attribute_B])
+        )
+        correlation = jStat.corrcoeff(attributeValues, targetValues).toFixed(2)
+      }
+      return correlation
     }
   }
 }
